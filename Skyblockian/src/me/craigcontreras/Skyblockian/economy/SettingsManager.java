47d9685439cfbb2a94bb91f6dfd2d18b191a3413
Nathaniel <1nrichp122@gmail.com>
2019-06-17 19:22:56 -0400

    Added UUID Support for Economy

--- Skyblockian/src/me/craigcontreras/Skyblockian/economy/SettingsManager.java
+++ Skyblockian/src/me/craigcontreras/Skyblockian/economy/SettingsManager.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 
@@ -40,24 +41,30 @@ public class SettingsManager
 	
 	public double getBalance(String p)
 	{
-		return config.getDouble("money." + p.toLowerCase());
+		String name = Bukkit.getPlayer(p).getUniqueId().toString();
+		return config.getDouble("money." + name);
 	}
 	
 	public void addBalance(String p, double amount)
 	{
-		setBalance(p, getBalance(p) + amount);
+		String name = Bukkit.getPlayer(p).getUniqueId().toString();
+		setBalance(name, getBalance(name) + amount);
 	}
 	
 	public boolean removeBalance(String p, double amount)
 	{
-		if (getBalance(p) - amount < 0) return false;
-		setBalance(p, getBalance(p) - amount);
+		String name = Bukkit.getPlayer(p).getUniqueId().toString();
+		
+		if (getBalance(name) - amount < 0) return false;
+		setBalance(name, getBalance(name) - amount);
 		return true;
 	}
 	
 	public void setBalance(String p, double amount)
 	{
-		config.set("money." + p.toLowerCase(), amount);
+		String name = Bukkit.getPlayer(p).getUniqueId().toString();
+		
+		config.set("money." + name, amount);
 		save();
 	}
 	
