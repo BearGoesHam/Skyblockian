e71f7e942405718637eb998c6f339a61e522702c
craigcontreras <craig1contreras@gmail.com>
2019-06-17 19:32:04 -0400

    Reverted economy

--- Skyblockian/src/me/craigcontreras/Skyblockian/economy/SettingsManager.java
+++ Skyblockian/src/me/craigcontreras/Skyblockian/economy/SettingsManager.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 
@@ -15,72 +14,66 @@ public class SettingsManager
 {
 	private static SettingsManager sm = new SettingsManager();
 	private SettingsManager() {}
-	
+
 	public static SettingsManager getEcoManager()
 	{
 		return sm;
 	}
-	
+
 	private FileConfiguration config;
 	private File cFile;
-	
+
 	public void setup(Skyblockian p)
 	{
 		if (!(p.getDataFolder().exists())) p.getDataFolder().mkdir();
-		
+
 		cFile = new File(p.getDataFolder(), "economy.yml");
-		
+
 		if (!(cFile.exists()))
 		{
 			try { cFile.createNewFile(); }
 			catch (Exception e) { e.printStackTrace(); }
 		}
-		
+
 		config = YamlConfiguration.loadConfiguration(cFile);
 	}
-	
+
 	public double getBalance(String p)
 	{
-		String name = Bukkit.getPlayer(p).getUniqueId().toString();
-		return config.getDouble("money." + name);
+		return config.getDouble("money." + p.toLowerCase());
 	}
-	
+
 	public void addBalance(String p, double amount)
 	{
-		String name = Bukkit.getPlayer(p).getUniqueId().toString();
-		setBalance(name, getBalance(name) + amount);
+		setBalance(p, getBalance(p) + amount);
 	}
-	
+
 	public boolean removeBalance(String p, double amount)
 	{
-		String name = Bukkit.getPlayer(p).getUniqueId().toString();
-		
-		if (getBalance(name) - amount < 0) return false;
-		setBalance(name, getBalance(name) - amount);
+		if (getBalance(p) - amount < 0) return false;
+		setBalance(p, getBalance(p) - amount);
 		return true;
 	}
-	
+
 	public void setBalance(String p, double amount)
 	{
-		String name = Bukkit.getPlayer(p).getUniqueId().toString();
-		
-		config.set("money." + name, amount);
+		config.set("money." + p.toLowerCase(), amount);
 		save();
 	}
-	
+
 	public ArrayList<String> getValues()
 	{
 		Map<String, Object> map = config.getValues(true);
 		ArrayList<String> lines = new ArrayList<>();
-		
+
 		for (Entry<String, Object> e : map.entrySet())
 		{
 			lines.add(e.getValue() + " " + e.getKey());
 		}
-		
+
 		return lines;
 	}
-	
+
 	private void save()
 	{
 		try { config.save(cFile); }
