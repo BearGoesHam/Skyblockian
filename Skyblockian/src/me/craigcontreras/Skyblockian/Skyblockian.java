24396de74d3c72c7601c52eed0dd0ce2dcfb3a24
craigcontreras <craig1contreras@gmail.com>
2019-06-23 21:02:01 -0400

    Changed the Core class

--- Skyblockian/src/me/craigcontreras/Skyblockian/Skyblockian.java
+++ Skyblockian/src/me/craigcontreras/Skyblockian/Skyblockian.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.logging.Level;
 
-import me.craigcontreras.Skyblockian.commands.*;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Difficulty;
@@ -22,6 +21,11 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 import com.sk89q.worldedit.bukkit.WorldEditPlugin;
 
+import me.craigcontreras.Skyblockian.commands.BountyCommand;
+import me.craigcontreras.Skyblockian.commands.CommandManagerAdmin;
+import me.craigcontreras.Skyblockian.commands.HelpCommand;
+import me.craigcontreras.Skyblockian.commands.IslandCommand;
+import me.craigcontreras.Skyblockian.commands.SpawnCommand;
 import me.craigcontreras.Skyblockian.commands.admin.FreezeCommand;
 import me.craigcontreras.Skyblockian.commands.admin.HitDelayCommand;
 import me.craigcontreras.Skyblockian.commands.admin.StaffModeCommand;
@@ -67,7 +71,7 @@ extends JavaPlugin
 	public WorldEditPlugin worldEdit;
 	private static Skyblockian skyBlockian;
 	private static WarpManager WarpManager;
-	
+		
 	public ArrayList<Player> toTeleportTo = new ArrayList<Player>();
 	public HashMap<Player, Double> Bounties = new HashMap<Player, Double>();
 	
@@ -122,7 +126,7 @@ extends JavaPlugin
 	                }
 	            }
 	        }.runTaskTimer(this, 20, 20);
-		}
+	    }
 	}
 	
 	public void onDisable()
