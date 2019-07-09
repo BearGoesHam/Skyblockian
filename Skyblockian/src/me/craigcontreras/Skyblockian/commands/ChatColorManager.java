947d8e9f8b8857624e48b7e530feeda802ab788a
Nathaniel <1nrichp122@gmail.com>
2019-07-09 15:39:14 -0400

    Added ChatColor Command, added ChatColor into ChatEvent

---
+++ Skyblockian/src/me/craigcontreras/Skyblockian/commands/ChatColorManager.java
@@ -0,0 +1,36 @@
+package me.craigcontreras.Skyblockian.commands;
+
+import org.bukkit.entity.Player;
+
+import me.craigcontreras.Skyblockian.Skyblockian;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class ChatColorManager 
+{
+
+
+
+	private static ChatColorManager instance = new ChatColorManager();
+
+	public Map<UUID, String> chatcolors = new HashMap<UUID, String>();
+
+	
+	public static ChatColorManager getChatColorManager()
+	{
+		return instance;
+	}
+	
+	public String getChatColorType(Player player)
+	{
+		//return Skyblockian.getCore().colorConfig.getString(player.getUniqueId().toString());
+	return this.chatcolors.get(player.getUniqueId()).toString();
+	}
+	
+	
+	
+	
+
+}
